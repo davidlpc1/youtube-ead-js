01fe7032a28fa2cfa245ce50c1fdcdc5857a9f3e
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-14 16:53:07 -0300

    We can register and login normal or even with facebook

--- db.js
+++ db.js
@@ -1,30 +1,95 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./ead.db');
-// const tables = require('./tables.json')
-// db.serialize(() => {
-//     tables.forEach(createTableCommand =>  db.run(createTableCommand))
-// })
+const urlForDatabase = './ead.db'
+const fs = require('fs')
+const databaseAlreadyExists = fs.existsSync(urlForDatabase)
 
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database(urlForDatabase)
 
-// db.findAll = () => {
-//     return new Promise((resolve,reject) => {
-//         db.all(`SELECT * FROM notes`,(error,rows) => {
-//             if(error) reject(error)
+const bcrypt = require('bcrypt')
+
+if (!databaseAlreadyExists) { 
+    const tables = require('./tables.json')
+    db.serialize(() => {
+        tables.forEach(createTableCommand =>  db.run(createTableCommand))
+    })
+}
+
+db.findUserByName = (username) => {
+    return new Promise((resolve,reject) => {
+        db.all(`SELECT * FROM USERS WHERE username = ?`,[username],(error,rows) => {
+            if(error) reject(error)
     
-//             resolve(rows)
-//         })
-//     })
-// }
+            resolve(rows)
+        })
+    })
+}
 
-// db.findOne = (id) => {
-//     return new Promise((resolve,reject) => {
-//         db.all(`SELECT * FROM notes WHERE id=?`,[id],(error,rows) => {
-//             if(error) reject(error)
+db.addUser = (username,password,image,about) => {
+    return new Promise(async(resolve, reject) => {
+        const saltRounds = 10
+        const usersWithThatName = await db.findUserByName(username)
+        if(usersWithThatName.length > 0){
+            reject({ message:'That username already exists' })
+        }
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if(err) reject('Error in criptografe your password')
+            bcrypt.hash(password, salt, (err, hash) => {
+                if(err) reject('Error in criptografe your password')
+                db.run("INSERT INTO users (username,hash,level,image,about) VALUES(?,?,1,?,?) ",
+                [username,hash,image,about],(error) => {
+                    if(error) reject(error)
+                })
+                resolve('Sucess')
+            })
+        })
+    })
+}
+
+db.findAllUsers = () => {
+    return new Promise((resolve,reject) => {
+        db.all(`SELECT * FROM USERS`,(error,rows) => {
+            if(error) {
+                reject(error)
+                throw new Error(error)
+            }
     
-//             resolve(rows)
-//         })
-//     })
-// }
+            resolve(rows)
+        })
+    })
+}
+
+db.deleteAllUsers = () => {
+    return new Promise((resolve,reject) => {
+        db.run(`DELETE FROM users`,error=>{
+            if(error) {
+                reject(error)
+                throw new Error(error)
+            }
+    
+            resolve('All users have been deleted')
+        })
+    })
+}
+
+db.loginUser = (username,password) => {
+    return new Promise(async (resolve,reject) => {
+        const user = await db.findUserByName(username)
+        if(user.length <= 0){
+            reject({ message:'User not found' })
+            throw new Error('User not found')
+        }
+        const { id,hash } = user[0]
+        bcrypt.compare(password, hash, (err, result) => {
+            if(err) reject(err)
+            if(result) resolve(id)
+        });
+    })
+}
+
+const debuging = false
+if(debuging){
+    db.deleteAllUsers().then(() => {})
+}
 
 // db.insertData = (data) => {
 //     return new Promise((resolve,reject) => {
@@ -33,23 +98,13 @@ const db = new sqlite3.Database('./ead.db');
 //             name,
 //             description
 //         )
-//         VALUES (?,?);
+//         VALUES (?,?)
 //         `
 
 //         db.run(query,data,error => {
 //             if (error) return reject(error)
     
 //             resolve(`I have inserted ${this}`)
-//         })
-//     })
-// }
-
-// db.deleteNote = (id) => {
-//     return new Promise((resolve,reject) => {
-//         db.run(`DELETE FROM notes WHERE id = ?`,[id],error=>{
-//             if(error) return reject(error)
-    
-//             resolve(`I have deleted the note with id ${id}`)
 //         })
 //     })
 // }
@@ -64,4 +119,4 @@ const db = new sqlite3.Database('./ead.db');
 //     })
 // }
 
-module.exports = db;
+module.exports = db
